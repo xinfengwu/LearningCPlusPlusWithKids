afc0097422adff63f8717857ea97fe9549e1f58d
Wu <jiller@126.com>
2020-11-20 16:05:58 +0800

    Create printPoemChunXiao.cpp

---
+++ printPoemChunXiao.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout<<"春晓"<<endl
+      <<"春眠不觉晓，"<<endl
+      <<"处处闻啼鸟。"<<endl
+      <<"夜来风雨声，"<<endl
+      <<"花落知多少。";
+  return 0;
+}
